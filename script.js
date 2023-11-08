415f10c157fefc5cda482c8d14f249b08e825445
MuhEdwin <148667364+MuhEdwin@users.noreply.github.com>
2023-11-08 11:52:35 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function login() {
+    var done = 0;
+    var username = document.login.username.value;
+    var password = document.login.password.value;
+
+    if(username == "himasi" && password == "himasi") {
+        if(confirm("Apakah ingin login?") == true){
+            window.location.href ="https://github.com/MuhEdwin";
+        } else{
+            alert("membatalkan login !");
+        }
+    } else{
+        if(done==0){
+            alert("username/password salah!");
+        }
+
+    }
+    
+}
